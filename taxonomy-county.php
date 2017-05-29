bfb26bb0547d60aefe993b67ea6145ce5f736f7a
nicholaslee <i@nicholasleegoodman.com>
2017-05-29 16:21:30 -0500

    Taxonomy County archive now with sidebar.

--- taxonomy-county.php
+++ taxonomy-county.php
@@ -7,20 +7,21 @@
  * @package pwmap
  */
 
-get_header(); ?>
+get_header('county'); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
 
 		<?php
 		if ( have_posts() ) : ?>
-
-			<header class="page-header">
+			
+			<!-- <header class="page-header">
 				<?php
-					the_archive_title( '<h1 class="page-title">', '</h1>' );
-					the_archive_description( '<div class="archive-description">', '</div>' );
+					//the_archive_title( '<h1 class="page-title">', '</h1>' );
+				//	the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
-			</header><!-- .page-header -->
+			
+			</header> --><!-- .page-header -->
 
 			<?php
 			/* Start the Loop */
@@ -47,5 +48,5 @@ get_header(); ?>
 	</div><!-- #primary -->
 
 <?php
-get_sidebar();
+//get_sidebar();
 get_footer();
--- template-parts/content.php
+++ template-parts/content.php
@@ -10,8 +10,10 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<header class="entry-header">
-		<?php
+
+
+	<div class="entry-content">
+	<?php
 		if ( is_single() ) :
 			the_title( '<h1 class="entry-title">', '</h1>' );
 		else :
@@ -24,9 +26,6 @@
 		</div><!-- .entry-meta -->
 		<?php
 		endif; ?>
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
 		<?php
 			the_content( sprintf(
 				/* translators: %s: Name of current post. */
