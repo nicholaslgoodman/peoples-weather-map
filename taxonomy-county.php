d90ce126fd6abbfac58b769cb4fc923868a3604b
nicholaslee <i@nicholasleegoodman.com>
2017-05-29 17:19:25 -0500

    Altered loop for taxonomy-county archive to contain excerpts instead of full posts.

--- taxonomy-county.php
+++ taxonomy-county.php
@@ -11,36 +11,26 @@ get_header('county'); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
-
 		<?php
-		if ( have_posts() ) : ?>
-			
-			<!-- <header class="page-header">
-				<?php
-					//the_archive_title( '<h1 class="page-title">', '</h1>' );
-				//	the_archive_description( '<div class="archive-description">', '</div>' );
+					the_archive_title( '<h1 class="page-title">', '</h1>' );
+					//the_archive_description( '<div class="archive-description">', '</div>' );
 				?>
+		<?php
+		if ( have_posts() ) : while ( have_posts() ) : the_post(); ?> 
+		
+				
 			
-			</header> --><!-- .page-header -->
-
+			 <!-- .page-header -->
 			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
-
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/content', get_post_format() );
-
-			endwhile;
-
-			the_posts_navigation();
-
-		else :
-
-			get_template_part( 'template-parts/content', 'none' );
+			the_title('<h1 class="post-title">', '</h1>');
+        	the_excerpt();
+        	?>
+        	<h5><a href="<?php the_permalink() ?>">Read the story</a></h5>
+        <?php
+    		endwhile;
+    	else :
+        	_e( 'Sorry, no posts matched your criteria.', 'textdomain' );
+   		
 
 		endif; ?>
 
@@ -48,5 +38,4 @@ get_header('county'); ?>
 	</div><!-- #primary -->
 
 <?php
-//get_sidebar();
 get_footer();
